e1678c8ae9172fa50cc8c76acbd3a28af58b6430
Reynardt Cloete <91493490+ReynardtCloete@users.noreply.github.com>
2025-05-24 17:01:10 +0200

    torch overlay

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
 
+
 //BURGER//////////////////////////////////////////////////////////////////////////
 
     const hamburger = document.querySelector('.hamburger');
@@ -16,6 +17,23 @@ const header = document.querySelector('header');
 window.addEventListener('scroll', () => {
   header.classList.toggle("sticky", window.scrollY > 0)
 })
-/////////////////////////////////////////////////////////////////////////////////
+
+//TORCH OVERLAY///////////////////////////////////////////////////////////////////
+
+const torchOverlay = document.getElementById('torch-overlay');
+
+document.addEventListener('mousemove', (e) => { //This function (e) => runs everytime the mouse moves//
+  const x = e.clientX;                          //Extracts cursor's position//
+  const y = e.clientY;                          //Extracts cursor's position//
+  torchOverlay.style.background = `
+    radial-gradient(circle at ${x}px ${y}px,
+      rgba(29, 46, 202, 0.3) 0px,
+      rgba(29, 46, 202, 0.25) 100px,
+      rgba(29, 46, 202, 0.15) 200px,
+      rgba(29, 46, 202, 0.1) 300px,
+      transparent 500px)
+  `;
+});
+
 
 });
