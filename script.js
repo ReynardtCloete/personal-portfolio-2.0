0ea42dde04d1a4d9697a91fd1c12c5f3d2ce1e87
Reynardt Cloete <91493490+ReynardtCloete@users.noreply.github.com>
2025-05-26 19:55:24 +0200

    added some JS

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
 
 
-//BURGER//////////////////////////////////////////////////////////////////////////
+//BURGER//
 
     const hamburger = document.querySelector('.hamburger');
     const navbar = document.querySelector('.navbar');
@@ -11,20 +11,21 @@ document.addEventListener('DOMContentLoaded', function () {
       navbar.classList.toggle('active');
     });
 
-//NAVBAR//////////////////////////////////////////////////////////////////////////
+//NAVBAR//
+
 const header = document.querySelector('header');
 
 window.addEventListener('scroll', () => {
   header.classList.toggle("sticky", window.scrollY > 0)
 })
 
-//TORCH OVERLAY///////////////////////////////////////////////////////////////////
+//TORCH-OVERLAY//
 
 const torchOverlay = document.getElementById('torch-overlay');
 
 document.addEventListener('mousemove', (e) => { //This function (e) => runs everytime the mouse moves//
-  const x = e.clientX;                          //Extracts cursor's position//
-  const y = e.clientY;                          //Extracts cursor's position//
+  const x = e.clientX; //Extracts cursor's position//
+  const y = e.clientY; //Extracts cursor's position//
   torchOverlay.style.background = `
     radial-gradient(circle at ${x}px ${y}px,
       rgba(29, 46, 202, 0.3) 0px,
@@ -35,5 +36,15 @@ document.addEventListener('mousemove', (e) => { //This function (e) => runs ever
   `;
 });
 
+//ANIMATIONS//
+
+const elements = [
+  ...document.querySelectorAll('.nav-link'),
+  document.querySelector('.resume'),
+];
+
+elements.forEach((el, i) => {
+  setTimeout(() => el.classList.add('visible'), i * 250);
+});
 
 });
