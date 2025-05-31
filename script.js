7c7ace09385ad63fd4e7acaeefe206af1231351d
Reynardt Cloete <91493490+ReynardtCloete@users.noreply.github.com>
2025-05-31 18:30:39 +0200

    I think I'm done here

--- script.js
+++ script.js
@@ -1,50 +1,57 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function () { ////////////////////////////////////////////////////////////////////////////////////
 
 
 //BURGER//
+const hamburger = document.querySelector('.hamburger');
+const navbar = document.querySelector('.navbar');
+const navLinks = document.querySelectorAll('.navbar a');
 
-    const hamburger = document.querySelector('.hamburger');
-    const navbar = document.querySelector('.navbar');
+hamburger.addEventListener('click', () => {
 
-    hamburger.addEventListener('click', () => {
-      hamburger.classList.toggle('active');
-      navbar.classList.toggle('active');
-    });
+  hamburger.classList.toggle('active');
+  navbar.classList.toggle('active');
+
+});
+
+navLinks.forEach(link => {
+
+  link.addEventListener('click', () => {
+
+    hamburger.classList.remove('active');
+    navbar.classList.remove('active');
+
+  });
+
+});
 
 //NAVBAR//
-
 const header = document.querySelector('header');
 
 window.addEventListener('scroll', () => {
+
   header.classList.toggle("sticky", window.scrollY > 0)
-})
+
+});
 
 //TORCH-OVERLAY//
-
 const torchOverlay = document.getElementById('torch-overlay');
 
 document.addEventListener('mousemove', (e) => { //This function (e) => runs everytime the mouse moves//
+  
   const x = e.clientX; //Extracts cursor's position//
   const y = e.clientY; //Extracts cursor's position//
+  
   torchOverlay.style.background = `
+
     radial-gradient(circle at ${x}px ${y}px,
-      rgba(29, 46, 202, 0.3) 0px,
-      rgba(29, 46, 202, 0.25) 100px,
-      rgba(29, 46, 202, 0.15) 200px,
-      rgba(29, 46, 202, 0.1) 300px,
-      transparent 500px)
+    rgba(29, 46, 202, 0.3) 0px,
+    rgba(29, 46, 202, 0.25) 100px,
+    rgba(29, 46, 202, 0.15) 200px,
+    rgba(29, 46, 202, 0.1) 300px,
+    transparent 500px)
+
   `;
+  
 });
 
-//ANIMATIONS//
-
-const elements = [
-  ...document.querySelectorAll('.nav-link'),
-  document.querySelector('.resume'),
-];
-
-elements.forEach((el, i) => {
-  setTimeout(() => el.classList.add('visible'), i * 250);
-});
-
-});
+});/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
